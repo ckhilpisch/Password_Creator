2fe19f48095b7af7238419ee04318e4fcee57eb4
CaroKris <ckhilpisch@gmail.com>
2021-01-05 23:15:00 -0600

    added variables

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var passCombo = "";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specChar = "~!@#$%^&*+|\/";
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
