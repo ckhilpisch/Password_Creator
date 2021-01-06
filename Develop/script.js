8074a2b04a4e48720ef7909bf0f4921c7d943724
CaroKris <ckhilpisch@gmail.com>
2021-01-05 23:58:18 -0600

    added a prompt for password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//declare an empty string to push all of the possible string combos to
 var passCombo = "";
+//create strings for the four variable types
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specChar = "~!@#$%^&*+|\/";
+//create an empty string to hold the password length
+var passLength = ''
+
+//ask user for a length between 8 and 128 and store the variable
+while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+  passLength = prompt ("Please select a number between 8 -128, my friend");
+
+}
+alert("You chose " + passLength + " for your password length");
+
+
+
+
 
 
 
